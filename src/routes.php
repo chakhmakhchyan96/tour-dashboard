01980c9d18331e7d0119be0699770285ff1f38cb
Shant Chakhmakhchyan <chakhmakhchyan96@gmail.com>
2019-06-28 11:15:33 +0400

    remove routes

--- src/routes.php
+++ src/routes.php
@@ -1,26 +1,13 @@
 <?php
-
-Route::get('test', function(){
-    echo 'Hello from the test package!';
-});
 //
-//Route::group(['middleware' => ['web']], function () {
+//Route::group(
+//    [
+//        'prefix' => \Mcamara\LaravelLocalization\Facades\LaravelLocalization::setLocale() .'/dashboard',
+//        'middleware' => ['localeSessionRedirect', 'localizationRedirect', 'localeViewPath', 'web']
+//    ],
 //
-//    Route::resource('cat', '\AISTGlobal\TourDashboard\TourCategoryController');
+//    function () {
 //
-//});
-
-Route::group(
-    [
-        'prefix' => \Mcamara\LaravelLocalization\Facades\LaravelLocalization::setLocale() .'/dashboard',
-        'middleware' => ['localeSessionRedirect', 'localizationRedirect', 'localeViewPath', 'web']
-    ],
-
-    function () {
-
-//        Route::get('/', ['as' => 'dashboard', 'uses' => 'DashboardController@index']);
-        Route::resource('tours', '\AISTGlobal\TourDashboard\TourController');
-        Route::resource('categories', '\AISTGlobal\TourDashboard\TourCategoryController');
-//        Route::get('/translations', ['as' => 'translation_manager', 'uses' => '\Barryvdh\TranslationManager\Controller@getIndex']);
-//        Route::get('/translations/view/{group?}', ['as' => 'translation_group', 'uses' => '\Barryvdh\TranslationManager\Controller@getView']);
-    });
+//        Route::resource('tours', '\AISTGlobal\TourDashboard\TourController');
+//        Route::resource('categories', '\AISTGlobal\TourDashboard\TourCategoryController');
+//    });
